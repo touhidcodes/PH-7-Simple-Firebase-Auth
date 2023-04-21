8ed98048146d9ade5827884e6b3bf2c107fcda43
Touhidur Zaman <touhidcodes@gmail.com>
2023-04-21 17:36:49 +0600

    set-popup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
 
 const Login = () => {
 	const auth = getAuth(app);
 	const provider = new GoogleAuthProvider();
+	const handleToAuth = () => {
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				const user = result.user;
+				console.log(user);
+			})
+			.catch((error) => {
+				const massage = error.code;
+				console.log("error", massage);
+			});
+	};
 	return (
 		<div>
 			<h1>This is login page</h1>
-			<button>Google Login</button>
+			<button onClick={handleToAuth}>Google Login</button>
 		</div>
 	);
 };
